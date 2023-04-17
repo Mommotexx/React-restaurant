fb2b883c2ec25dd3829a3696ee1861adfa69674c
Marcus Vinje Johansen <tex@duck.com>
2023-04-17 23:34:55 +0200

    Added and styled header component

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from "react";
 
-import './Header.css';
+import { images } from "../../constants";
+import { SubHeading } from "../../components";
+import "./Header.css";
 
 const Header = () => (
-  <div>
-    Header
-  </div>
+    <header className="app__header app__wrapper section__padding" id="home">
+        <div className="app__wrapper_info">
+            <SubHeading title="Chase the new flavor" />
+            <h1 className="app__header-h1">The Key to Fine Dining</h1>
+            <p className="p__opensans" style={{ margin: "2rem 0" }}>
+                Doloremque, autem sed? Veritatis similique enim culpa asperiores
+                est aliquid amet labore, ipsum libero, molestias consectetur.
+                Facere atque nobis error facilis vel!
+            </p>
+            <button type="button" className="custom__button">
+                Explore Menu
+            </button>
+        </div>
+        <div className="app__wrapper_img">
+            <img src={images.welcome} alt="Header img" />
+        </div>
+    </header>
 );
 
 export default Header;
