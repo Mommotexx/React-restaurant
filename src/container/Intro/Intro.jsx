7b3529eb8ea0818b46f676ceef119ccdd2ec5679
Marcus Vinje Johansen <tex@duck.com>
2023-04-19 15:51:41 +0200

    Added the video on the page, and it's style and controls

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useState } from "react";
+import { BsFillPlayFill, BsPauseFill } from "react-icons/bs";
 
-import './Intro.css';
+import { meal } from "../../constants";
+import "./Intro.css";
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+const Intro = () => {
+    const [playVideo, setPlayVideo] = useState(false);
+    const vidRef = React.useRef();
+
+    const handleVideo = () => {
+        setPlayVideo(!playVideo);
+
+        if (playVideo) {
+            vidRef.current.pause();
+        } else {
+            vidRef.current.play();
+        }
+    };
+
+    return (
+        <section className="app__video" onClick={handleVideo}>
+            <video
+                src={meal}
+                type="video/mp4"
+                loop
+                controls={false}
+                ref={vidRef}
+                muted
+            />
+            <div
+                className="app__video-overlay flex__center"
+                style={playVideo ? { display: "none" } : { display: "flex" }}
+            >
+                <div
+                    className="app__video-overlay_circle flex__center"
+                    onClick={handleVideo}
+                >
+                    {playVideo ? <BsPauseFill /> : <BsFillPlayFill />}
+                </div>
+            </div>
+        </section>
+    );
+};
 
 export default Intro;
